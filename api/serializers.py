9271e8a65ad936d8fe1619b5f5a60765982d401f
Ikshan Bhardwaj <ikshanbhardwaj.2003@gmail.com>
2022-11-21 23:38:32 +0530

    package.json updated

--- api/serializers.py
+++ api/serializers.py
@@ -5,19 +5,19 @@ from .models import Equipment, Court, Timeslot
 class EquipmentSerializer(ModelSerializer):
     class Meta:
         model = Equipment
-        feilds = "__all__"
+        fields = "__all__"
         exclude = []
 
 
 class CourtSerializer(ModelSerializer):
     class Meta:
         model = Court
-        feilds = "__all__"
+        fields = "__all__"
         exclude = []
 
 
 class TimeslotSerializer(ModelSerializer):
     class Meta:
         model = Timeslot
-        feilds = "__all__"
+        fields = "__all__"
         exclude = []
