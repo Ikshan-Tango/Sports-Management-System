3ab6bf1487a3759b7f8037f215e5f57f327893b8
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-11-25 16:11:54 +0530

    equipments page amended

--- Client/src/Components/pages/Equipment book/bookequipments.js
+++ Client/src/Components/pages/Equipment book/bookequipments.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import {Component} from 'react'
 import Navigation from '../Navigation/Navigation'
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,52 +8,60 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button } from 'react-bootstrap'
+import axios from 'axios'
 
-function IssueEquipments() {
-  const [equipmentName, setEquipmentName] = React.useState();
-  const [quantity, setQuantity] = React.useState();
-  const formSubmitHandler = async(e) => {
-}
+class IssueEquipments extends Component {
+  constructor(props){
+    super(props)
 
-function createData(
-  name: string,
-  quantity: number,
-) {
-  return { name, quantity};
-}
+    this.state={
+      equipments: [],
+      errorMsg: ""
+    }
+  }
 
-const rows = [
-  createData('Tennis', 15),
-  createData('Basketball', 237),
-  createData('Cricket Bat', 262),
-  createData('Football', 305),
-  createData('My feelings for her :(', 3000),
-];
+  componentDidMount(){
+    axios.get("http://localhost:8000/api/equipments/")
+    .then(response => {
+      console.log(response)
+      this.setState({
+        equipments: response.data
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      this.setState({
+        errorMsg: "Error retrieving data"
+      })
+    })
+  }
+  render(){
+  const {equipments, errorMsg} = this.state
   return (
     <>
     <Navigation />
     <div className="card" style={{marginTop:50}}>
-    <center><h1 style={{marginTop:60}}>Issue equipments for use</h1></center>
+    <center><h1 style={{marginTop:60}}>Equipments availability for use</h1></center>
+    <center><h6>(Don't forget to bring your Thapar ID Cards/ Faculty IDs while you come to issue equipments)</h6></center>
     <div>
 <TableContainer component={Paper}>
       <Table stickyHeader sx={{ minWidth: 650 }} aria-label="sticky table">
         <TableHead>
           <TableRow>
-            <TableCell >Equipment Name</TableCell>
+            <TableCell>Equipment Name</TableCell>
             <TableCell align="center">Quantity Available</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {equipments.map((equipments) => (
             <TableRow
-              key={row.name}
+              key={equipments.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {equipments.name}
               </TableCell>
-              <TableCell align="center">{row.quantity}</TableCell>
+              <TableCell align="center">{equipments.number}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -63,5 +72,5 @@ const rows = [
     </>
   )
 }
-
+}
 export default IssueEquipments
